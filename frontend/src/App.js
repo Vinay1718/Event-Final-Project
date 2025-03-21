6dbb4480d37ab7cc68a529d6f1dd2eb8f9a49955
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-03-21 20:08:16 +0530

    onfiguration updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sidebar from './component/sidebar/Sidebar';
+import Home from './component/Home/Home';
+import Settings from './component/Settings/Settings';
+import Orders from './component/Orders/Orders';
+import Cart from './component/Cart/Cart';
+import Payments from './component/Payments/Payments';
+import Support from './component/Support/Support';
+import Tickets from './component/Tickets/Tickets';
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Sidebar />
-    </BrowserRouter>
+      <BrowserRouter>
+        <div className="app-container">
+          <Sidebar />
+          <div className="content-container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/settings' element={<Settings />} />
+              <Route path='/orders' element={<Orders />} />
+              <Route path='/cart' element={<Cart />} />
+              <Route path='/payments' element={<Payments />} />
+              <Route path='/support' element={<Support />} />
+              <Route path='/tickets' element={<Tickets />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
